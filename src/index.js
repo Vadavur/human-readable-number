a15c44ef8d196d9d455eec3f0005165fbae215f0
Vadavur <vadavur@gmail.com>
2021-03-14 17:43:55 +0300

    init: implement number translation function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function toReadable (number) {
+  const digits = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const decades = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  const numArr = (`${number}`).split('').reverse();
+  let returnArr = [];
   
+  if (number == 0) {return 'zero'};
+
+  if (numArr.length == 3){
+    returnArr.push(digits[numArr[2]] + " hundred");
+  }
+
+  if (numArr[1] == 1){
+    returnArr.push(teens[numArr[0]]);
+  } 
+  else if (numArr[1] > 1){
+    returnArr.push(decades[numArr[1]]);
+    if (numArr[0] != 0) {
+      returnArr.push(digits[numArr[0]]);
+    }
+  } 
+  else {
+    if (numArr[0] != 0) {
+      returnArr.push(digits[numArr[0]]);
+    }
+  }
+
+  return returnArr.join(' ');
 }
